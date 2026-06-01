502d27d54d3712770a46807307288d7c80dcff26
Matthew Stibbard <matthew@stibbard.au>
2026-06-01 11:46:47 +1000

    gh stack

--- stacks/github.ts
+++ stacks/github.ts
@@ -17,6 +17,13 @@ export default Alchemy.Stack(
   },
   Effect.gen(function* () {
     const accountId = yield* Config.string("CLOUDFLARE_ACCOUNT_ID").pipe(Effect.orDie);
+    const planetscaleApiTokenId = yield* Config.string("PLANETSCALE_API_TOKEN_ID").pipe(
+      Effect.orDie,
+    );
+    const planetscaleApiToken = yield* Config.string("PLANETSCALE_API_TOKEN").pipe(Effect.orDie);
+    const planetscaleOrganization = yield* Config.string("PLANETSCALE_ORGANIZATION").pipe(
+      Effect.orDie,
+    );
 
     const apiToken = yield* Cloudflare.AccountApiToken("CIToken", {
       accountId,
@@ -32,6 +39,8 @@ export default Alchemy.Stack(
             "Pages Write",
             "Account Settings Write",
             "Workers Tail Read",
+            "Secrets Store Write",
+            "Hyperdrive Write",
           ],
           resources: {
             [`com.cloudflare.api.account.${accountId}`]: "*",
@@ -52,6 +61,27 @@ export default Alchemy.Stack(
       repository: GITHUB_REPO,
       name: "CLOUDFLARE_ACCOUNT_ID",
       value: Redacted.make(accountId),
+    });
+
+    yield* GitHub.Secret("planetscale-api-token-id", {
+      owner: GITHUB_OWNER,
+      repository: GITHUB_REPO,
+      name: "PLANETSCALE_API_TOKEN_ID",
+      value: Redacted.make(planetscaleApiTokenId),
+    });
+
+    yield* GitHub.Secret("planetscale-api-token", {
+      owner: GITHUB_OWNER,
+      repository: GITHUB_REPO,
+      name: "PLANETSCALE_API_TOKEN",
+      value: Redacted.make(planetscaleApiToken),
+    });
+
+    yield* GitHub.Secret("planetscale-organization", {
+      owner: GITHUB_OWNER,
+      repository: GITHUB_REPO,
+      name: "PLANETSCALE_ORGANIZATION",
+      value: Redacted.make(planetscaleOrganization),
     });
   }),
 );
